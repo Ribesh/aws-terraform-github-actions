e74bcd87021b7659af8440a9a26da592fee880fd
Ribesh <ribeshshr@gmail.com>
2026-02-17 22:20:28 +0545

    fix: updated the tf files

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ data "aws_ami" "ubuntu_ami" {
 resource "aws_instance" "my_ubuntu_ec2" {
     ami = data.aws_ami.ubuntu_ami.id
     instance_type = "t3a.micro"
-    key_name = "Ribesh_KeyPair_Main.pem"
+    key_name = "Ribesh_KeyPair_Main"
     subnet_id = data.aws_subnet.default.id
     vpc_security_group_ids = [aws_security_group.dast_sg.id]
 
