badfb3c3d2cbc101617f302a6384727787b67878
Ribesh <ribeshshr@gmail.com>
2026-02-17 22:29:17 +0545

    fix: added 3000 port in SG

--- sg.tf
+++ sg.tf
@@ -10,6 +10,13 @@ resource "aws_security_group" "dast_sg" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+    ingress {
+        to_port = 3000
+        from_port = 3000
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
     egress {
         from_port = 0
         to_port = 0
