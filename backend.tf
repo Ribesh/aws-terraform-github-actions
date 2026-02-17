d915135d8ae25054baabdc20cb815d4a23d4c7a8
Ribesh <ribeshshr@gmail.com>
2026-02-17 22:18:56 +0545

    fix: updated the tf files

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-ribesh-state-bucket"
-    key            = "dast/terraform.tfstate"
+    key            = "ec2/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
   }
--- sg.tf
+++ sg.tf
@@ -7,13 +7,13 @@ resource "aws_security_group" "dast_sg" {
         from_port = 22
         to_port = 22
         protocol = "tcp"
-        cidr_block = ["0.0.0.0/0"]
+        cidr_blocks = ["0.0.0.0/0"]
     }
 
     egress {
         from_port = 0
         to_port = 0
         protocol = "-1"
-        cidr_block = ["0.0.0.0/0"]
+        cidr_blocks = ["0.0.0.0/0"]
     }
 }
